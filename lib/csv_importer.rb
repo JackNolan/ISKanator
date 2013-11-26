8b28b56c83a23fb6c54afa3a435df72fa649e3d3
Jack Nolan <jack.nolan@code42.com>
2013-11-26 15:56:09 -0600

    csv importer and spec

--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class CsvImporter
   attr_accessor :file_name
 
@@ -5,11 +7,26 @@ class CsvImporter
     @file_name = file_name
   end
 
+  def import!(dest_class)
+    hashed_csv.each do |row|
+      dest_class.create(row)
+    end
+  end
+
   def file
     @file ||= File.new(file_name)
   end
 
   def csv
-    file.read
+    @csv ||= CSV.parse file
+  end
+
+  def hashed_csv
+    headers = csv.shift
+    csv.map do |row|
+      headers.zip(row).each_with_object({}) do |(header, value), row_hash|
+        row_hash[header] = value
+      end
+    end
   end
 end
--- spec/lib/csv_importer_spec.rb
+++ spec/lib/csv_importer_spec.rb
@@ -2,12 +2,13 @@ require "spec_helper"
 
 describe CsvImporter do
   let(:csv_file_name) { "test_file.csv" }
-  let(:file_path)     { Rails.root.join("spec/cassettes/csv/#{csv_file_name}") }
+  let(:file_path)     { Rails.root.join("spec/fixtures/csv/#{csv_file_name}") }
   let(:importer)      { CsvImporter.new(file_path) }
 
-  describe ".import!" do
-    it "" do
-
+  describe ".import!(const_name)" do
+    it "loads the csv into the correct table" do
+      Item.should_receive(:create).once
+      importer.import! Item
     end
   end
 
@@ -22,9 +23,17 @@ describe CsvImporter do
   end
 
   describe ".csv" do
-    let(:csv) { "header_one,header_two,header_three\nvalue_one,value_two,value_three\n" }
+    let(:csv) { [["header_one", "header_two", "header_three"], ["value_one", "value_two", "value_three"]] }
+
     it "returns the correct csv" do
       expect(importer.csv).to eq csv
+    end
+  end
+
+  describe ".hashed_csv" do
+    let(:hashed_csv) { [{ 'header_one' => 'value_one', 'header_two' => 'value_two', 'header_three' => 'value_three' }] }
+    it "returns the hashed version of csv" do
+      expect(importer.hashed_csv).to eq hashed_csv
     end
   end
 end
